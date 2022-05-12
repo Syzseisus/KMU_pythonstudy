0b22110755cda5eba6ed862b92982d19520ba77a
tkddnjs98 <89904421+tkddnjs98@users.noreply.github.com>
2022-05-12 23:49:36 +0900

    Create Sangwon.py

---
+++ 220516/344. Reverse String/Sangwon.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        i = 0
+        j = len(s)-1
+        def reverse(i, j, s):
+            if i >= j:
+                return 
+            s[i], s[j] = s[j], s[i]
+            reverse(i+1, j-1, s)
+        reverse(i, j, s)
